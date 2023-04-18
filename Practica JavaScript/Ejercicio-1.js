36eb6d413aa0c62e3c5b18ddf2e0329cc30ca9a2
Tomas Birbe <tomas.birbe@gmail.com>
2023-04-18 18:39:41 -0300

    simplificar resolucion del ejercicio 1 de javascript

--- Practica JavaScript/Ejercicio-1.js
+++ Practica JavaScript/Ejercicio-1.js
@@ -12,35 +12,16 @@ const names = [
 function golfScore(par, strokes) {
 	if (strokes === 1) {
 		return names[0]
-	} 
+	}
 
 	if (strokes <= par - 2) {
 		return names[1]
 	}
 
-	if (strokes === par - 1) {
-		return names[2]
+	if (strokes >= par + 3) {
+		return names[names.length - 1]
 	}
 
-	if (strokes === par) {
-		return names[3]
-	}
+	return names[strokes - par + 3]
 
-		if (strokes === par + 2) {
-		return names[4]
-	}
-
-	if (strokes === par + 1) {
-		return names[5]
-	}
-
-	return names[6]
 }
-
-console.log(golfScore(15, 4))
-console.log(golfScore(15, 15))
-console.log(golfScore(15, 17))
-console.log(golfScore(15, 13))
-console.log(golfScore(15, 16))
-console.log(golfScore(15, 14))
-console.log(golfScore(15, 50))
