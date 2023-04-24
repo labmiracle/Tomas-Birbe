eec81803c8a2f520a3a0493bdf9f18292b59cdf5
Tomas Birbe <tomas.birbe@gmail.com>
2023-04-24 12:41:22 -0300

    realizar el punto 2 del ejercicio 12 de la practica de javascript

--- Practica JavaScript/Ejercicio-12.js
+++ Practica JavaScript/Ejercicio-12.js
@@ -5,17 +5,66 @@ const prompt = readline.createInterface({ input, output });
 
 async function game() {
 	let gameFinished = false;
-
+	let changeDifficulty = true;
 	while (!gameFinished) {
-		const response = await prompt.question('Ingresa un nro del 1 al 10 \n> ')
-		if (response === 'exit') {
+		console.clear()
+		console.log("Escribir 'exit' para salir o 'cambiar dificultad' para cambiar de dificultad")
+		while (changeDifficulty) {
+			difficultyResponse = await prompt.question(`Seleccionar dificultad 
+1- Del 0 al 10
+2- Del 0 al 50
+3- Del 0 al 100
+> `
+			)
+
+			switch (difficultyResponse) {
+				case "1": {
+					difficulty = 10
+					changeDifficulty = false
+					break
+				}
+				case "2": {
+					difficulty = 50
+					changeDifficulty = false
+					break
+				}
+				case "3": {
+					difficulty = 100
+					changeDifficulty = false
+					break
+				}
+				case 'exit': {
+					gameFinished = true;
+					changeDifficulty = false
+					break
+				}
+				default: {
+					console.log('Ingresar un numero del 1 al 3')
+					changeDifficulty = true
+					break
+				}
+			}
+		}
+
+		if (gameFinished) {
+			prompt.close()
+			break;
+		}
+
+		const guessResponse = await prompt.question(`Ingresa un nro del 1 al ${difficulty} \n> `)
+		if (guessResponse === 'cambiar dificultad') {
+			changeDifficulty = true
+			continue;
+		}
+		if (guessResponse === 'exit') {
 			gameFinished = true
+			prompt.close()
 		} else {
-			const num = Number(response)
+			const num = Number(guessResponse)
 
 			if (num) {
 				console.clear()
-				const secretNumber = Math.round(Math.random() * 10)
+				const secretNumber = Math.round(Math.random() * difficulty)
 				if (num === secretNumber) {
 					console.log('Acertaste!')
 				} else {
@@ -24,7 +73,6 @@ async function game() {
 			} else {
 				console.log('Tiene que ser un numero!')
 			}
-
 		}
 	}
 }
