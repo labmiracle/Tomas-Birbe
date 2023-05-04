fb4d7c4b48889dbad61af72761644916009f1b7c
Tomas Birbe <tomas.birbe@gmail.com>
2023-05-04 19:49:31 -0300

    realizar ejercicio 2.4 de la practica de TypeScript

---
+++ TypeScript/Ejercicios/src/Ejercicio-2.4.ts
@@ -0,0 +1,58 @@
+interface EggLayer {
+    layEggs(): void;
+}
+interface Flyer {
+    fly(height: number): void;
+}
+interface Swimmer {
+    swim(depth: number): void;
+}
+// agregar alias de tipo(s) aqui
+
+type BirdLike = EggLayer & Flyer;
+type FishLike = Swimmer & EggLayer;
+type Animal = Bird | Fish;
+
+class Bird implements BirdLike {
+    constructor(public species: string) {}
+
+    layEggs(): void {
+        console.log("Poniendo huevos de aves.");
+    }
+
+    fly(height: number): void {
+        console.log(`Volando a la altura de ${height} metros.`);
+    }
+}
+
+class Fish implements FishLike {
+    constructor(public species: string) {}
+
+    layEggs(): void {
+        console.log("Poniendo huevos de pescado.");
+    }
+
+    swim(depth: number): void {
+        console.log(`Nadando a una profundidad de ${depth} metros.`);
+    }
+}
+
+function getRandomAnimal(): Animal {
+    const animals = [new Bird("puffin"), new Bird("kittiwake"), new Fish("sea robin"), new Fish("leafy seadragon")];
+
+    return animals[Math.floor(Math.random() * animals.length)];
+}
+
+function interrogateAnimal(animal = getRandomAnimal()): string {
+    if (animal instanceof Fish) {
+        animal.swim(10); // se llama solo si es un pez
+    }
+
+    if (animal instanceof Bird) {
+        animal.fly(10); // se llama solo si es un pajaro
+    }
+
+    return animal.species;
+}
+
+console.log("[Ejercicio 4.4]", `Tenemos un ${interrogateAnimal()} en nuestras manos!`);
