1e9edd5894760d9e3be90a0a111b321459c76145
Tomas Birbe <tomas.birbe@gmail.com>
2023-06-03 11:22:46 -0300

    realizar ejercicio map

---
+++ Programacion-Funcional/src/map.js
@@ -0,0 +1,10 @@
+import { characters } from './data.js';
+
+const names = characters.map((character) => character.male);
+const heights = characters.map((character) => character.height);
+const namesAndHeights = characters.map((character) => ({
+  name: character.male,
+  height: character.height,
+}));
+
+console.log({ names, heights, namesAndHeights });
