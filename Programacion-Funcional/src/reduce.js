5f413223779bfcec3240adfa32ab0f4413fae4c3
Tomas Birbe <tomas.birbe@gmail.com>
2023-06-03 11:24:09 -0300

    realizar ejercicio reduce

---
+++ Programacion-Funcional/src/reduce.js
@@ -0,0 +1,30 @@
+import { characters } from './data.js';
+
+const totalMass = characters.reduce(
+  (acumulatedMass, character) => acumulatedMass + Number(character.mass),
+  0
+);
+
+const totalHeight = characters.reduce(
+  (acumulatedHeight, character) => acumulatedHeight + Number(character.height),
+  0
+);
+
+const totalLenghtOfCharactersName = characters.reduce(
+  (accumulatedCharacters, character) =>
+    accumulatedCharacters + character.male.length,
+  0
+);
+
+const totalLenghtOfCharacterEyes = characters.reduce(
+  (accumulatedCharacters, character) =>
+    accumulatedCharacters + character.eye_color.length,
+  0
+);
+
+console.log({
+  totalMass,
+  totalHeight,
+  totalLenghtOfCharactersName,
+  totalLenghtOfCharacterEyes,
+});
