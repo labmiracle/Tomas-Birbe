caa8894c25246f2c90b18681e60fb245a3027ce4
Tomas Birbe <tomas.birbe@gmail.com>
2023-06-08 19:08:02 -0300

    arreglar map.js

--- Programacion-Funcional/src/map.js
+++ Programacion-Funcional/src/map.js
@@ -1,6 +1,6 @@
 import { characters } from './data.js';
 
-const names = characters.map((character) => character.male);
+const names = characters.map((character) => character.name);
 const heights = characters.map((character) => character.height);
 const namesAndHeights = characters.map((character) => ({
   name: character.male,
