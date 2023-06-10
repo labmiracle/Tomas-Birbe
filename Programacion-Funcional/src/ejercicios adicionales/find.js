722a55a69c51f545fa0de75ef260c19772fc9c65
Tomas Birbe <tomas.birbe@gmail.com>
2023-06-10 08:51:04 -0300

    agregar export a la funcion find

--- Programacion-Funcional/src/ejercicios adicionales/find.js
+++ Programacion-Funcional/src/ejercicios adicionales/find.js
@@ -1,13 +1,15 @@
-import { characters } from '../data.js'
+import { characters } from '../data.js';
 
-function find(arr, func) {
-	for(let i = 0; i < arr.length; i++) {
-		if(func(arr[i])) {
-			return arr[i]
-		}
-	}
+export function find(arr, func) {
+  for (let i = 0; i < arr.length; i++) {
+    if (func(arr[i])) {
+      return arr[i];
+    }
+  }
 }
 
-const character = find(characters, (character) => character.name.includes('Leia'))
+const character = find(characters, (character) =>
+  character.name.includes('Leia')
+);
 
-console.log(character)
+console.log(character);
