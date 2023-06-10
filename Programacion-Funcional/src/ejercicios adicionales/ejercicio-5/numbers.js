8f2fcd2dbf59311c6f722f7a9704e8a6c3ebb491
Tomas Birbe <tomas.birbe@gmail.com>
2023-06-10 11:59:39 -0300

    agregar estructura para el ejercicio 5 de los ejercicios adicionales

---
+++ Programacion-Funcional/src/ejercicios adicionales/ejercicio-5/numbers.js
@@ -0,0 +1,32 @@
+export const numbers = {
+  zero() {
+    return 0;
+  },
+  one() {
+    return 1;
+  },
+  two() {
+    return 2;
+  },
+  three() {
+    return 3;
+  },
+  four() {
+    return 4;
+  },
+  five() {
+    return 5;
+  },
+  six() {
+    return 6;
+  },
+  seven() {
+    return 7;
+  },
+  eight() {
+    return 8;
+  },
+  nine() {
+    return 9;
+  },
+};
---
+++ Programacion-Funcional/src/ejercicios adicionales/ejercicio-5/operators.js
@@ -0,0 +1,6 @@
+export const operators = {
+  plus() {},
+  minus() {},
+  times() {},
+  dividedBy() {},
+};
