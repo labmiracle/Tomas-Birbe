826f1541cd4a9190c3900ee62d4d569e31595ef5
Tomas Birbe <tomas.birbe@gmail.com>
2023-06-11 18:47:08 -0300

    realizar punto 1 de la seccion State

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -1,9 +1,13 @@
 import { Props } from './Props';
+import { Counter } from './State';
 
 function App() {
   return (
     <>
+      <h1>Props</h1>
       <Props></Props>
+      <h2>State</h2>
+      <Counter></Counter>
     </>
   );
 }
