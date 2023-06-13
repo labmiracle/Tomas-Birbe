220dfab6d05e7a723e1d380a9a390f349ef5ba24
Tomas Birbe <tomas.birbe@gmail.com>
2023-06-13 17:55:21 -0300

    limpiar componente RandomText por codigo duplicado

--- React/src/Events/RandomText.tsx
+++ React/src/Events/RandomText.tsx
@@ -1,19 +1,10 @@
 import { useState } from 'react';
-import { Todo } from './Todo';
-
-type Todo = {
-  userId: number;
-  id: number;
-  title: string;
-  completed: boolean;
-};
 
 export function RandomText() {
   const [text, setText] = useState('');
 
   return (
     <>
-      <Todo></Todo>
       <p>Escribi cualquier texto!</p>
       <p>{text}</p>
       <label htmlFor="randomText">Texto </label>
